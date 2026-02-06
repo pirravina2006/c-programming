22bc8814528ee6f4e82d0b0c7f0238eee614567b
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-02-06 21:06:12 +0530

    loop20.c

--- While loop/loop20.c
+++ While loop/loop20.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
 int main(){
-    int c;
-    scanf("%d",&c);
-    int n;
-    scanf("%d",&n);
-    int i=1;
     int a;
-    int cr=0;
-    while(i<=n){
-        scanf("%d",&a);
-        c=c+a;
-        if(c>100){
-            cr++;
+    scanf("%d",&a);
+    int n;
+    int i=1;
+    int p,c;
+    int dr=0;
+    int count=0;
+    int cr=-1;
+    while(i<=a){
+        scanf("%d",&c);
+        if(c<p){
+            dr=dr+1;
+            count=count+1;
         }
+        else{
+            count=0;
+        }
+        if(count==3 && cr==-1){
+            cr=i;
+        }
+        p=c;
         i++;
     }
-    printf("Final beds:%d\n",100);
-    printf("Critical hours:%d",cr);
+    if(cr>-1){
+        printf("Crash days :%d",cr);
+    }
+    else{
+        printf("No crash detected");
+    }
+    printf("Total drops : %d",dr);
 }
