1b2c78966ba547ad120386ac81341317b8ba1e2a
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-02-08 21:04:26 +0530

    Add files via upload

---
+++ 2D array/arr23.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int r;
+    int c;
+    scanf("%d",&r);
+    scanf("%d",&c);
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int sum=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(i==0 || i==r-1 ||j==0 || j==c-1){
+                sum=sum+a[i][j];
+            }
+        }
+    }
+    printf("%d",sum);
+}
